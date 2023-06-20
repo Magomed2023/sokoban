3638613dfc8161279449831f3307f336b127f9bf
Magomed2023 <m.muzhakhoev@gmx.at>
2023-06-20 20:19:44 +0200

    NextStep

---
+++ assets/levels-readme.md
@@ -0,0 +1,18 @@
+# license
+
+levels borrowed from:  
+http://sokoban.org/list.php  
+and  
+http://borgar.net/programs/sokoban/#Sokoban
+
+# level elements
+
+@ = player on floor
++ = player on target
+$ = box on floor
+* = box on target
+# = wall
+. = goal
+- = floor inside (dash)
+_ = floor outside (underscore)
+two linebreaks (\n\n) = level separator
--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 // SERVER
 
 import express from 'express';
+import fs from 'node:fs/promises';
+
+const levels = [];
+
+const levelData = await fs.readFile('./assets/levels.txt', 'utf8');
+// console.log(levelData);
+
+function readLevelData() {
+    
+    // Schritt 1: levelData in einzelne Levels zerlegen
+
+    const splitLevelData = levelData.split("\n\n");
+    
+
+    // Schritt 2: für jedes Level die Daten zeichen für zeichen durchgehen
+
+
+    // und in gewünschte Struktur übertragen
+
+    for (let singleLevelData of splitLevelData) {
+        const level = {
+            level: [],
+            player: {},
+            boxes: [],
+        };
+
+        const lines = singleLevelData.split('\n');
+        for (let y = 0; y < lines.length; y += 1) {
+            const line = [];
+            for (let x = 0; x < lines[y].length; x += 1) {
+                const char = lines[y][x];
+
+                switch (char) {
+                    case '@':
+                        line.push('-');
+                        level.player.x = x;
+                        level.player.y = y;
+            
+                        break;
+
+                    case '+':
+                        line.push('.');
+                        level.player.x = x;
+                        level.player.y = y;
+                        break;
+
+                    case '$':
+                        line.push('-');
+                        level.boxes.push({x, y});
+                        break;
+
+                    case '*':
+                        line.push('.');
+                        level.boxes.push({x, y});
+                        break;
+
+                    default: //# . - _
+                        line.push(char);
+                        break;
+                }
+            }
+            level.level.push(line);
+        }
+        levels.push(level);
+    }
+}
+readLevelData();
+console.log(levels[0]);
+
 const app = express();
 const port = 8080;
 
